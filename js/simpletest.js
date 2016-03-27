f81c2841126e8efadb6a020a9beda8530c2c1c27
trumko <trumko@gmail.com>
2016-03-27 19:42:34 +0300

    Add Ajax requests to wheel-of-fortune, code refactoring

--- js/simpletest.js
+++ js/simpletest.js
@@ -1,81 +1,83 @@
 $.when(
-  $.getJSON("api/simple-test_Beatles.json"), // get json
+  $.getJSON("api/simple-test_Beatles.json"), // get json with quastions
   $.get("templ/simple-test-quastions.html"), // get template with quastions page
   $.ajax("templ/simple-test-final.html") // get template with final page
   )
   .then(renderPage)
-  .fail(function(){console.log("Error")
+  .fail(function(){console.log("Error");
 });
 
 
+function renderPage(DataRequest, qTmplRequest, finTmplRequest) {
 
+  var quastions = DataRequest[0]; // data from parsed JSON
+  var qTmpl = qTmplRequest[0];  // template with quastions page
+  var finTmpl = finTmplRequest[0];  // template with final page
 
-function renderPage(quastionData, qTmpl, finTmpl) {
+  var simpleQuiz = $('#simple-quiz'); // container for rendered info
+  var quastionCounter = 0; // number of current quastion
+  var correctAnswers = 0; //counter of the correct answers
 
-    var quastions = quastionData[0]; // quastions from parsed JSON
+  var quastionPageTmpl = _.template(qTmpl);  // underscore template for quastion page
+  var finalnPageTmpl = _.template(finTmpl);  // underscore template for final page
 
-    var quastionCounter = 0; // number of current quastion
-    var simpleQuiz = $('#simple-quiz'); // container for rendered info
-    var correctAnswers = 0; //counter of the correct answers
+  // render function for quastion page
+  function renderQuastionPage(template, arr, index) {
+    var info = template({
+                        quastion: arr[index].quastion,
+                        options: arr[index].option
+                       });
+    $('#simple-quiz').html(info);
+  }
 
-    // underscore template for quastion page
-    var quastionPageTmpl = _.template(qTmpl[0]);
+  // render function for final page
+  function renderFinalPage(template, arr) {
+    var info = template({ 
+                        result: correctAnswers,
+                        totalQuastion: arr.length
+                       });
+    $('#simple-quiz').html(info);
+  }
 
-    // underscore template for final page
-    var finalnPageTmpl = _.template(finTmpl[0]);
+  // event function when click option
+  function optionChooseEvent() {
 
+    var optionId = ( ($(this).attr('id')) ); // ID that is clicked on
 
-    // render function for quastion page
-    function renderQuastionPage(template, arr, index) {
-      var info = template({quastion: arr[index].quastion, options: arr[index].option});
-      $('#simple-quiz').html(info);
-    };
-
-    // render function for quastion page
-    function renderFinalPage(template, arr) {
-      var info = template({result: correctAnswers, totalQuastion: arr.length});
-      $('#simple-quiz').html(info);
-    };
-
-    // event function when click option
-    function optionChooseEvent() {
-
-      var optionId = ( ($(this).attr('id')) ); // ID that is clicked on
-
-     if (optionId == quastions[quastionCounter].correct) {
-        $(this).addClass('option-correct'); //make it green
-        $(this).children().removeClass('glyphicon-unchecked').addClass('glyphicon-ok') //change icon;
-        $(this).siblings().addClass('option-not-active'); //make other options not active
-        correctAnswers++;
-        console.log(correctAnswers);
-      } else {
-        $(this).addClass('option-wrong');
-        $(this).siblings().addClass('option-not-active'); //make other options not active
-        $(this).children().removeClass('glyphicon-unchecked').addClass('glyphicon-remove')
-        $("#" + quastions[quastionCounter].correct).removeClass('option-not-active').addClass('option-correct'); //make it green
-        $("#" + quastions[quastionCounter].correct).children().removeClass('glyphicon-unchecked').addClass('glyphicon-ok');
-      }
-
-      $( "#simple-quiz").undelegate( '#options li', 'click', optionChooseEvent); //undelegate event
+    if (optionId == quastions[quastionCounter].correct) {
+      $(this).addClass('option-correct'); //make it green
+      $(this).children().removeClass('glyphicon-unchecked').addClass('glyphicon-ok'); //change icon;
+      $(this).siblings().addClass('option-not-active'); //make other options not active
+      correctAnswers++;
+      console.log(correctAnswers);
+    } else {
+      $(this).addClass('option-wrong');
+      $(this).siblings().addClass('option-not-active'); //make other options not active
+      $(this).children().removeClass('glyphicon-unchecked').addClass('glyphicon-remove');
+      $("#" + quastions[quastionCounter].correct).removeClass('option-not-active').addClass('option-correct'); //make it green
+      $("#" + quastions[quastionCounter].correct).children().removeClass('glyphicon-unchecked').addClass('glyphicon-ok');
     }
 
-    // event function when click next button
-    function nextPageEvent() {
-      quastionCounter++;
-      if (quastionCounter < quastions.length) {
-        // delegate click event each time new page is loaded
-        $( "#simple-quiz").delegate( '#options li', 'click', optionChooseEvent);
-        //simpleQuiz.html(''); // if add this one we receive empty table at the end 
-        renderQuastionPage(quastionPageTmpl, quastions, quastionCounter);
-      } else {
-        renderFinalPage(finalnPageTmpl, quastions)
-      };
+    $( "#simple-quiz").undelegate( '#options li', 'click', optionChooseEvent); //undelegate event
+  }
+
+  // event function when click next button
+  function nextPageEvent() {
+    quastionCounter++;
+    if (quastionCounter < quastions.length) {
+      // delegate click event each time new page is loaded
+      $( "#simple-quiz").delegate( '#options li', 'click', optionChooseEvent);
+      //simpleQuiz.html(''); // if add this one we receive empty table at the end 
+      renderQuastionPage(quastionPageTmpl, quastions, quastionCounter);
+    } else {
+      renderFinalPage(finalnPageTmpl, quastions);
     }
+  }
 
-    // choose option event
-    $( "#simple-quiz").delegate( '#options li', 'click', optionChooseEvent);
-    // next button event
-    $( "#simple-quiz").delegate( ".next-button", 'click', nextPageEvent);
+  // choose option event
+  $( "#simple-quiz").delegate( '#options li', 'click', optionChooseEvent);
+  // next button event
+  $( "#simple-quiz").delegate( ".next-button", 'click', nextPageEvent);
 
-   renderQuastionPage(quastionPageTmpl, quastions, quastionCounter)
+  renderQuastionPage(quastionPageTmpl, quastions, quastionCounter);
 }
--- js/wheel-of-fortune.js
+++ js/wheel-of-fortune.js
@@ -1,111 +1,99 @@
-// List of quastions 
-var quastions = [{
-  quastion: 'Основний літературний твір ірландського письменника Джеймса Джойса',
-  answer: 'Улісс'
-}, {
-  quastion: 'Яке прізвище автора казки "Маленький принц"?',
-  answer: 'Екзюпері'
-}, {
-  quastion: 'Країна, у якій народився відомий письменник та поет Хорхе Луїс Борхес?',
-  answer: 'Аргентина'
-}, {
-  quastion: 'Як звати хлопця від імені якого ведеться розповідь у романі "Мобі Дік" Германа Мелвілла?',
-  answer: 'Ізмаїл'
-}, {
-  quastion: 'За чиїм романом було знято фільм "Втеча з Шоушенку"?',
-  answer: 'Кінг'
-}];
-
-var simpleQuiz = $('#simple-quiz'); // container for rendered info
-var quastionCounter = 0; // number of current quastion
-var correctAnswers = 0; // number of current aswers
-var hints = 0; // total number of letter hints
-var currentHints = 0; // number of letter hints on current word
+$.when(
+  $.getJSON("api/wheel-of-fortune_LiteratureXX.json"), // get json with quastions
+  $.get("templ/wheel-of-fortune-quastions.html"), // get template with quastions page
+  $.get("templ/wheel-of-fortune-final.html") // get template with final page
+  )
+  .then(renderPage)
+  .fail(function(){console.log("Error");
+});
 
 
-// underscore template for quastion page
-var quastionPageTmpl = _.template('<div class="block-quastion"><h1><%= quastion %></h1></div>\
-  <div id="options" class="block-options"><div class="block-letters">\
-  <% _.each(answ, function(option, iterator)\
-  {%> <div class="card-wrap"><div id="card" class="card-block"><div class="front-side">?</div><div class="back-side"><%= option %></div></div></div> <%})%>\
-  </div><div class="form-group wheel-answer"><input type="text" class="form-control check-answer" placeholder="answer"></div>\
-  </div><div class="next-button"><a href="#"><span>next</span></a></div>');
+function renderPage(DataRequest, qTmplRequest, finTmplRequest) {
 
-// underscore template for final page
-var finalnPageTmpl = _.template('<div class="final-info"><span>Ви дали <%= result %> з <%= totalQuastion %> правильних відповідей і скористались <%= usedHints %> підказками</span></div><div class="next-button"><a href="index.html"><span>back to main page</span></a></div>');
+  var quastions = DataRequest[0]; // data from parsed JSON
+  var qTmpl = qTmplRequest[0];  // template with quastions page
+  var finTmpl = finTmplRequest[0];  // template with final page
 
+  var simpleQuiz = $('#simple-quiz'); // container for rendered info
+  var quastionCounter = 0; // number of current quastion
+  var correctAnswers = 0; // number of current aswers
+  var hints = 0; // total number of letter hints
+  var currentHints = 0; // number of letter hints on current word
 
-// render function for quastion page
-function renderQuastionPage(template, arr, index) {
-  var info = template({
-                       quastion: arr[index].quastion,
-                       answ: arr[index].answer.toUpperCase().split("")
-                     });
-  $('#simple-quiz').html(info);
-};
+  var quastionPageTmpl = _.template(qTmpl); // underscore template for quastion page
+  var finalnPageTmpl = _.template(finTmpl); // underscore template for final page
 
-// render function for final page
-function renderFinalPage(template, arr) {
-  var info = template({
-                       result: correctAnswers,
-                       totalQuastion: arr.length,
-                       usedHints: hints
-                     });
-  $('#simple-quiz').html(info);
-};
-
-// event function when click next button
-function nextPageEvent() {
-  quastionCounter++;
-  if (quastionCounter < quastions.length) {
-    renderQuastionPage(quastionPageTmpl, quastions, quastionCounter);
-  } else {
-    renderFinalPage(finalnPageTmpl, quastions)
+  // render function for quastion page
+  function renderQuastionPage(template, arr, index) {
+    var info = template({
+                         quastion: arr[index].quastion,
+                         answ: arr[index].answer.toUpperCase().split("")
+                       });
+    $('#simple-quiz').html(info);
   };
-  currentHints = 0;
-}
 
-// rotate the card with hide letter
-function flipCardEvent() {
-  hints++;
-  currentHints++;
-  if (currentHints == quastions[quastionCounter].answer.split("").length) {
+  // render function for final page
+  function renderFinalPage(template, arr) {
+    var info = template({
+                         result: correctAnswers,
+                         totalQuastion: arr.length,
+                         usedHints: hints
+                       });
+    $('#simple-quiz').html(info);
+  };
+
+  // event function when click next button
+  function nextPageEvent() {
+    quastionCounter++;
+    if (quastionCounter < quastions.length) {
+      renderQuastionPage(quastionPageTmpl, quastions, quastionCounter);
+    } else {
+      renderFinalPage(finalnPageTmpl, quastions)
+    };
+    currentHints = 0;
+  }
+
+  // rotate the card with hide letter
+  function flipCardEvent() {
+    hints++;
+    currentHints++;
+    if (currentHints == quastions[quastionCounter].answer.split("").length) {
+      $("input.check-answer").prop('disabled', true);
+    };
+    $( this ).parent().toggleClass('flipped');
+  }
+
+  // check answer
+  function checkAnswerEvent(e) {
+    if (e.keyCode == 13) {
+        $('.card-block').addClass('flipped');
+        checkCorrect();
+    }
+    $(this).undelegate( 'input.check-answer', 'keyup', checkAnswerEvent );
+  }
+
+  //check if answer is correct
+  function checkCorrect() {
+    var userAnsw = $('input.check-answer').val().toUpperCase();
+    var correctAnsw = quastions[quastionCounter].answer.toUpperCase();
     $("input.check-answer").prop('disabled', true);
-  };
-  $( this ).parent().toggleClass('flipped');
+    if (userAnsw === correctAnsw) {
+      correctAnswers++;
+      $('input.check-answer').addClass('correct-answer');
+    } else {
+      $('input.check-answer').addClass('wrong-answer');
+    };
+  }
+
+  // next button event
+  $( "#simple-quiz").delegate( ".next-button", 'click', nextPageEvent);
+
+  // flip card event
+  $( "#simple-quiz").delegate( '.front-side', 'click', flipCardEvent );
+
+  // check if the aswer is corect event
+  $( "#simple-quiz").delegate( 'input.check-answer', 'keyup', checkAnswerEvent );
+
+  // render page
+  renderQuastionPage(quastionPageTmpl, quastions, quastionCounter);
 }
-
-// check answer
-function checkAnswerEvent(e) {
-      if (e.keyCode == 13) {
-          $('.card-block').addClass('flipped');
-          checkCorrect();
-      }
-      $(this).undelegate( 'input.check-answer', 'keyup', checkAnswerEvent );
-}
-
-//check if answer is correct
-function checkCorrect() {
-  var userAnsw = $('input.check-answer').val().toUpperCase();
-  var correctAnsw = quastions[quastionCounter].answer.toUpperCase();
-  $("input.check-answer").prop('disabled', true);
-  if (userAnsw === correctAnsw) {
-    correctAnswers++;
-    $('input.check-answer').addClass('correct-answer');
-  } else {
-    $('input.check-answer').addClass('wrong-answer');
-  };
-}
-
-
-// next button event
-$( "#simple-quiz").delegate( ".next-button", 'click', nextPageEvent);
-
-// flip card event
-$( "#simple-quiz").delegate( '.front-side', 'click', flipCardEvent );
-
-// check if the aswer is corect event
-$( "#simple-quiz").delegate( 'input.check-answer', 'keyup', checkAnswerEvent );
-
-// render page
-renderQuastionPage(quastionPageTmpl, quastions, quastionCounter);
